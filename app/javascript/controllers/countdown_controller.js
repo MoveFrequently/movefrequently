93c39c5c4b7a2cec75c0b80303bc081a75843019
Miguel Fernandez <miguel@mff.io>
2025-01-13 11:03:05 +0100

    Fix webworker delaying count

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -3,55 +3,78 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["countdown", "exercise", "countdownElement"];
 
-  updateCountdown() {
+  connect() {
     const countdownContainer = this.countdownTarget;
     const exerciseContainer = this.exerciseTarget;
     const countdownElement = this.countdownElementTarget;
 
-    const now = new Date().getTime();
-    const diff = this.exerciseTime - now;
+    const exerciseTime = new Date(
+      this.element.getAttribute("data-time")
+    ).getTime();
 
-    if (diff <= 0) {
-      countdownContainer.style.display = "none";
-      exerciseContainer.style.display = "flex";
-      document.title = "LiveLonger - Time to Exercise!";
+    const updater = this.#updater(
+      countdownContainer,
+      exerciseContainer,
+      countdownElement,
+      exerciseTime
+    );
 
-      if (Notification.permission === "granted") {
-        new Notification("Time to Exercise!", {
-          body: "Your next exercise is ready",
-          icon: "/icon.png",
-        });
-      }
+    updater();
+    this.interval = setInterval(updater, 1000);
+  }
 
-      const audio = new Audio(
-        "https://www.myinstants.com/media/sounds/bereal.mp3"
-      );
-      audio
-        .play()
-        .catch((error) => console.error("Error playing sound:", error));
-      return;
+  disconnect() {
+    if (this.interval) {
+      clearInterval(this.interval);
     }
+  }
 
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  #update(countdownElement, dueIn) {
+    const minutes = Math.floor((dueIn % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((dueIn % (1000 * 60)) / 1000);
 
     const timeString = `${minutes.toString().padStart(2, "0")}:${seconds
       .toString()
       .padStart(2, "0")}`;
     countdownElement.textContent = timeString;
-    document.title = `LiveLonger - Next in ${timeString}`;
   }
 
-  connect() {
-    this.exerciseTime = new Date(
-      this.element.getAttribute("data-time")
-    ).getTime();
+  #finish(countdownContainer, exerciseContainer) {
+    countdownContainer.style.display = "none";
+    exerciseContainer.style.display = "flex";
+    document.title = "LiveLonger - Time to Exercise!";
 
-    updateCountdown();
-    this.interval = setInterval(updateCountdown, 1000);
-  }
+    if (Notification.permission === "granted") {
+      new Notification("Time to Exercise!", {
+        body: "Your next exercise is ready",
+        icon: "/icon.png",
+      });
+    }
 
-  disconnect() {
+    const audio = new Audio(
+      "https://www.myinstants.com/media/sounds/bereal.mp3"
+    );
+    audio.play().catch((error) => console.error("Error playing sound:", error));
     clearInterval(this.interval);
+
+    return;
+  }
+
+  #updater(
+    countdownContainer,
+    exerciseContainer,
+    countdownElement,
+    exerciseTime
+  ) {
+    return function () {
+      const now = new Date().getTime();
+      const dueIn = exerciseTime - now;
+
+      if (dueIn <= 0) {
+        this.#finish(countdownContainer, exerciseContainer);
+      } else {
+        this.#update(countdownElement, dueIn);
+      }
+    }.bind(this);
   }
 }
