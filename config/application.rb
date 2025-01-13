c075f91172e869b3c7ac0aeab0de5c1ed1567e36
Miguel Fernandez <miguel@mff.io>
2025-01-13 19:43:40 +0100

    Rename to movefrequently.com

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Livelonger
+module MoveFrequently
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -7,6 +7,6 @@ class ExercisesControllerTest < ActionDispatch::IntegrationTest
     create_exercise
     get join_exercises_url
     assert_response :success
-    assert_match "1 person is using Livelonger right now", response.body
+    assert_match "1 person is going to exercise with you", response.body
   end
 end
