0e9ebe4504f603a0dc2813f2c69a68f0b8a69ae3
Miguel Fernandez <miguel@mff.io>
2025-01-14 18:41:20 +0100

    Capture turbo rails events

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+document.addEventListener(
+  "turbo:load",
+  function (event) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-7536HQ48JM", { page_location: event.detail.url });
+  },
+  false
+);
