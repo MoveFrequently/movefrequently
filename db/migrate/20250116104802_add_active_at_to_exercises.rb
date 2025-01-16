474b491a02dad124d123b6abb38467be8f90f052
Miguel Fernandez <miguel@mff.io>
2025-01-16 18:19:44 +0100

    Activate a few exercises

--- db/migrate/20250116104802_add_active_at_to_exercises.rb
+++ db/migrate/20250116104802_add_active_at_to_exercises.rb
@@ -1,6 +1,14 @@
 class AddActiveAtToExercises < ActiveRecord::Migration[8.1]
-  def change
+  def up
     add_column :exercises, :active_at, :datetime
     add_index :exercises, :active_at
+    Exercise.unscoped.limit(10).each do |exercise|
+      exercise.update(active_at: Time.now.utc)
+    end
+  end
+
+  def down
+    remove_index :exercises, :active_at
+    remove_column :exercises, :active_at
   end
 end
